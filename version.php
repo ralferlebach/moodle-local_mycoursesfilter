09386ead93ebf82cd5a9c8023fbcc23b30ed8593
ralferlebach <ralf.erlebach@gmx.de>
2026-04-09 11:14:57 +0200

    Update version.php

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_mycoursesfilter';
 $plugin->version   = 2026040905;
 $plugin->requires  = 2022041900; // Moodle 4.0+.
-$plugin->release   = '1.2.0';
+$plugin->release   = '1.1.0';
 $plugin->maturity  = MATURITY_STABLE;
